88f43a83f7ca860d2c45d66eed51de7aa53495f3
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2024-05-10 21:44:04 +0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -184,8 +184,8 @@ class CarInterface(CarInterfaceBase):
     elif candidate in TSS2_CAR or ret.enableGasInterceptor:
       tune.kpBP = [0., 5., 20.]
       tune.kpV = [1.3, 1.0, 0.7]
-      tune.kiBP = [0., 5., 12., 20., 27.]
-      tune.kiV = [.35, .23, .20, .17, .1]
+      tune.kiBP = [ 0.,  12.,  20., 27., 40.]
+      tune.kiV =  [.35, .215, .195, .10, .01]
       if candidate in TSS2_CAR:
         ret.vEgoStopping = 0.25
         ret.vEgoStarting = 0.25
