a5be87b1337ff4d54b3e992488cc154f3ebd63b7
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2024-05-12 10:49:23 +0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -175,7 +175,7 @@ class CarInterface(CarInterfaceBase):
       tune.kpV = [1.3, 1.0, 0.7]
       # In MPH  = [  0,   27,   45,  60,  89]
       tune.kiBP = [ 0.,  1.,   12.,  20., 27., 40.]
-      tune.kiV =  [.325, .32,  .225, .20, .10, .01]
+      tune.kiV =  [.333, .33,  .225, .20, .10, .01]
       if candidate in TSS2_CAR:
         ret.vEgoStopping = 0.1         # car is near 0.1 to 0.2 when car starts requesting stopping accel
         ret.vEgoStarting = 0.1         # needs to be > or == vEgoStopping
@@ -187,7 +187,7 @@ class CarInterface(CarInterfaceBase):
       tune.kpBP = [0., 5., 20.]
       tune.kpV = [1.3, 1.0, 0.7]
       tune.kiBP = [ 0., 1.,  12.,  20., 27., 40.]
-      tune.kiV =  [.325, .32, .225, .20, .10, .01]
+      tune.kiV =  [.333, .33, .225, .20, .10, .01]
       if candidate in TSS2_CAR:
         ret.vEgoStopping = 0.25
         ret.vEgoStarting = 0.25
