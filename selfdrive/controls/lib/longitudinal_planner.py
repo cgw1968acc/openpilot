a46975e2f1cce0c5451322203aedc0c590882064
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2024-09-13 13:06:10 -0700

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -20,7 +20,7 @@ LON_MPC_STEP = 0.2  # first step is 0.2s
 A_CRUISE_MIN = -1.2
 A_CRUISE_MAX_VALS =   [2.0, 2.0, 1.75, 0.94, .58,  .46,  .34,  .12]
 A_CRUISE_MAX_BP =     [0.,  5.5,  8.,   11.,  20.,  25.,  30.,  40.]
-A_CRUISE_MIN_V =       [-0.020, -0.020, -0.020, -0.18,  -1.2, -1.2]
+A_CRUISE_MIN_V =       [-0.04, -0.04, -0.10, -0.28,  -1.2, -1.2]
 A_CRUISE_MIN_BP =      [0.,     0.05,   5.0,    12.,    18.,   30.]
 CONTROL_N_T_IDX = ModelConstants.T_IDXS[:CONTROL_N]
 
