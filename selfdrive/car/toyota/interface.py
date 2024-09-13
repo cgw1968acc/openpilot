99030b079699463edfc3a25195af58cdd247738f
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2024-09-13 12:12:14 -0700

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -149,16 +149,16 @@ class CarInterface(CarInterfaceBase):
         tune.kiV = [1.2]             # appears to produce minimal oscillation on TSS-P
     elif params.get_bool("FrogsGoMooTune"):
       ret.stopAccel = -2.5           # on stock Toyota this is -2.5
-      ret.stoppingDecelRate = 0.1    # reach stopping target smoothly
-      ret.vEgoStarting = 0.15
-      ret.vEgoStopping = 0.15
+      ret.stoppingDecelRate = 0.3    # reach stopping target smoothly
+      ret.vEgoStarting = 0.10
+      ret.vEgoStopping = 0.10
       tune.kiV = [1.0]
     elif candidate in TSS2_CAR or ret.enableGasInterceptor:
       tune.kpV = [0.0]
       tune.kiV = [0.5]
       if candidate in TSS2_CAR:
-        ret.vEgoStopping = 0.25
-        ret.vEgoStarting = 0.25
+        ret.vEgoStopping = 0.10
+        ret.vEgoStarting = 0.10
         ret.stoppingDecelRate = 0.3  # reach stopping target smoothly
     else:
       tune.kiBP = [0., 5., 35.]
